eefe9bb5cb3c277b858d757c1e910de959529e51
Ehab Mansour <81818344+ehabmansour1@users.noreply.github.com>
2021-10-27 07:36:40 +0200

    Add files via upload

--- javascript/season5-eps.js
+++ javascript/season5-eps.js
@@ -6,7 +6,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(1).mp4?alt=media&token=6bed6a36-2c9d-4559-93d2-b1d8d5c93f51";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(1).mp4?alt=media&token=2f4b4d32-7b9b-4445-8ec2-6debb7396c20";
     video.play();
   }
   {
@@ -21,7 +21,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(2).mp4?alt=media&token=907aa1d5-ead3-4128-9e72-e8f9a6447e36";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(2).mp4?alt=media&token=5dc7be84-68de-4f40-991c-b079f04b876d";
     video.play();
   }
   {
@@ -36,7 +36,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(3).mp4?alt=media&token=0ff6e584-386a-4084-a5bb-fc39e979ef24";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(3).mp4?alt=media&token=cbc93448-c893-4198-83d7-2d535428101d";
     video.play();
   }
   {
@@ -51,7 +51,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(4).mp4?alt=media&token=605deb06-a973-4c87-9407-4797ce3bf20a";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(4).mp4?alt=media&token=3c7d08c9-f98b-4c8e-af7e-7dcced894ffc";
     video.play();
   }
   {
@@ -66,7 +66,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(5).mp4?alt=media&token=dda852e7-dad3-4c6f-a1e5-f17e573513f0";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(5).mp4?alt=media&token=787c4d7e-3e55-4cad-a9b7-b3ee2ff62493";
     video.play();
   }
   {
@@ -81,7 +81,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(6).mp4?alt=media&token=618b9e46-4fd1-4361-ab35-2e4a6f847af3";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(6).mp4?alt=media&token=3e17e602-fb88-43d2-8dbd-a58d908bcede";
     video.play();
   }
   {
@@ -96,7 +96,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(7).mp4?alt=media&token=50578bc5-cff1-486b-aefb-13dd48b7541c";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(7).mp4?alt=media&token=d5d2ad6a-391c-4bfc-aa9c-924acef0793c";
     video.play();
   }
   {
@@ -111,7 +111,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(8).mp4?alt=media&token=d4402c20-05e4-4ca1-86db-dff29e708c03";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(8).mp4?alt=media&token=9179eec2-3036-4357-886e-81c6ca81a6a3";
     video.play();
   }
   {
@@ -126,7 +126,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(9).mp4?alt=media&token=6aba4af8-7bf4-45a3-851c-e9b66891e777";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(9).mp4?alt=media&token=4036d040-011a-4eb3-aedb-63b09ab2b970";
     video.play();
   }
   {
@@ -141,7 +141,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(10).mp4?alt=media&token=3da585e9-268c-4f6e-9819-82e90137236d";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(10).mp4?alt=media&token=1cb1317c-4f9f-46f3-bb01-62571e5c4e02";
     video.play();
   }
   {
@@ -156,7 +156,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(11).mp4?alt=media&token=8bbbaf9d-0861-4026-a785-d7fccce03dc1";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(11).mp4?alt=media&token=5b9129fe-0e1d-4b82-87ae-d174730fcbbf";
     video.play();
   }
   {
@@ -171,7 +171,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(12).mp4?alt=media&token=ad31a1b5-719a-40db-ad6c-793888180e58";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(12).mp4?alt=media&token=3d93efda-86b8-4a76-8289-90856bea657d";
     video.play();
   }
   {
@@ -186,7 +186,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(13).mp4?alt=media&token=79b25517-8b76-4c3a-93bf-6db993ffc20d";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(13).mp4?alt=media&token=592d14ce-cf14-477f-b73d-f86961ed2b63";
     video.play();
   }
   {
@@ -201,7 +201,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(14).mp4?alt=media&token=1193e204-efef-4be3-a7d5-108ca683815f";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(14).mp4?alt=media&token=12505b61-bce7-47af-8d64-735047d5883f";
     video.play();
   }
   {
@@ -216,7 +216,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(15).mp4?alt=media&token=bf0bb543-4c39-4f79-a2f6-e6a7f93822f8";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(15).mp4?alt=media&token=ed7779db-e288-4eb4-9c23-961ccb139d9b";
     video.play();
   }
   {
@@ -231,7 +231,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(16).mp4?alt=media&token=d26e2ef1-5e7e-4694-b7c1-b5135bf82656";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(16).mp4?alt=media&token=c3f3024b-5118-41b5-8cf1-631f8075259d";
     video.play();
   }
   {
@@ -246,7 +246,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(17).mp4?alt=media&token=68eee56c-b55d-4697-aec9-6e73377cffd2";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(17).mp4?alt=media&token=aa7f247e-83ca-4eaf-9753-58f93eef896d";
     video.play();
   }
   {
@@ -261,7 +261,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(18).mp4?alt=media&token=94489f60-0b7c-4778-93d4-6446682e6cae";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(18).mp4?alt=media&token=c605fa8f-6d37-4c55-b829-0c7c8490628b";
     video.play();
   }
   {
@@ -291,7 +291,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(20).mp4?alt=media&token=32c63c92-fc9a-48ce-8a86-476fb3d57531";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(20).mp4?alt=media&token=a996d703-efb9-4bf6-9d98-41a35dcadf05";
     video.play();
   }
   {
@@ -306,7 +306,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(21).mp4?alt=media&token=8336bc7a-5e69-4a19-9a09-732e60f6a13e";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(21).mp4?alt=media&token=24757de8-aa30-4fd5-8273-a071eea8a563";
     video.play();
   }
   {
@@ -321,7 +321,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(22).mp4?alt=media&token=51f392f2-478c-4654-9c26-54b39b8bd5d6";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(22).mp4?alt=media&token=17f9cdf5-3b11-4f3c-b3fb-b92c6de57a11";
     video.play();
   }
   {
@@ -336,7 +336,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(23).mp4?alt=media&token=7abebd82-3141-43b6-bccc-df92dbb3cfc9";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(23).mp4?alt=media&token=87284258-4b8f-4d21-9805-a7b9c5c9c9d9";
     video.play();
   }
   {
@@ -351,7 +351,7 @@ span.onclick = function () {
   });
   {
     video.src =
-      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S01E%20%20(24).mp4?alt=media&token=e95dc27a-ae19-473e-b851-d88e500d4b4a";
+      "https://firebasestorage.googleapis.com/v0/b/linen-creek-254220.appspot.com/o/Friends.CimaRed.BluRay.720P.S02E%20%20(24).mp4?alt=media&token=2834e80e-c285-46d9-9570-1c56c79de0a9";
     video.play();
   }
   {
